780c3a677f7a11c410965958e73df5ca41dda6d1
Leonardo <fanger53@gmail.com>
2021-06-08 19:07:11 -0500

    add preload assets to the main menu and maunin scene

--- src/Js/game.js
+++ src/Js/game.js
@@ -1,27 +1,26 @@
 import '../assets/style.scss';
 import Phaser from 'phaser';
+import SceneMainMenu from './sceneMainMenu';
+import SceneMain from './sceneMain';
+import SceneGameOver from './sceneGameOver';
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 480,
   height: 600,
-  scene: {
-      preload: preload,
-      create: create,
-      update: update
-  }
+  parent: 'board-game',
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [SceneMainMenu,
+    SceneMain,
+    SceneGameOver],
+  pixelArt: true,
+  roundPixels: true
 };
 
 const game = new Phaser.Game(config);
 
-function preload () {
-
-}
-
-function create () {
-
-}
-
-function update () {
-  
-}
