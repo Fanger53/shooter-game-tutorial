41d76094668314d71ca91498bc3da1697fb1d80f
Leonardo <fanger53@gmail.com>
2021-06-08 22:06:28 -0500

    add enemies, player lasers, enemy lasers, frustum culling, and collisions

--- src/Js/sceneMain.js
+++ src/Js/sceneMain.js
@@ -1,4 +1,4 @@
-import Player from './entities';
+import {Player, ChaserShip, CarrierShip, GunShip, PlayerLaser} from './entities';
 import sprBg0 from '../assets/img/sprBg0.png';
 import sprBg1 from '../assets/img/sprBg1.png';
 import sprExplosion from '../assets/img/sprExplosion.png';
@@ -93,23 +93,160 @@ this.load.audio('sndLaser', sndLaser);
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    this.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        var enemy = null;
+
+    if (Phaser.Math.Between(0, 10) >= 3) {
+      enemy = new GunShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+    else if (Phaser.Math.Between(0, 10) >= 5) {
+      if (this.getEnemiesByType("ChaserShip").length < 5) {
+
+        enemy = new ChaserShip(
+          this,
+          Phaser.Math.Between(0, this.game.config.width),
+          0
+        );
+      }
+    }
+    else {
+      enemy = new CarrierShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+
+    if (enemy !== null) {
+      enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+      this.enemies.add(enemy);
+    }
+      },
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(this.playerLasers, this.enemies, function(playerLaser, enemy) {
+      if (enemy) {
+        if (enemy.onDestroy !== undefined) {
+          enemy.onDestroy();
+        }
+        enemy.explode(true);
+        playerLaser.destroy();
+      }
+    });
+
+    this.physics.add.overlap(this.player, this.enemies, function(player, enemy) {
+      if (!player.getData("isDead") &&
+          !enemy.getData("isDead")) {
+        player.explode(false);
+        enemy.explode(true);
+      }
+    });
+
+    this.physics.add.overlap(this.player, this.enemyLasers, function(player, laser) {
+      if (!player.getData("isDead") &&
+          !laser.getData("isDead")) {
+        player.explode(false);
+        laser.destroy();
+      }
+    });
   }
 
   update() {
-    this.player.update();
+    if (!this.player.getData("isDead")) {
+      this.player.update();
+      if (this.keyW.isDown) {
+        this.player.moveUp();
+      }
+      else if (this.keyS.isDown) {
+        this.player.moveDown();
+      }
+      if (this.keyA.isDown) {
+        this.player.moveLeft();
+      }
+      else if (this.keyD.isDown) {
+        this.player.moveRight();
+      }
+    
+      if (this.keySpace.isDown) {
+        this.player.setData("isShooting", true);
+      }
+      else {
+        this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+        this.player.setData("isShooting", false);
+      }
+    }
 
-if (this.keyW.isDown) {
-  this.player.moveUp();
-}
-else if (this.keyS.isDown) {
-  this.player.moveDown();
-}
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
 
-if (this.keyA.isDown) {
-  this.player.moveLeft();
-}
-else if (this.keyD.isDown) {
-  this.player.moveRight();
-}
+      if (enemy.x < -enemy.displayWidth ||
+        enemy.x > this.game.config.width + enemy.displayWidth ||
+        enemy.y < -enemy.displayHeight * 4 ||
+        enemy.y > this.game.config.height + enemy.displayHeight) {
+    
+        if (enemy) {
+          if (enemy.onDestroy !== undefined) {
+            enemy.onDestroy();
+          }
+    
+          enemy.destroy();
+        }
+    
+    }
+
+      enemy.update();
+    }
+
+    for (var i = 0; i < this.enemyLasers.getChildren().length; i++) {
+      var laser = this.enemyLasers.getChildren()[i];
+      laser.update();
+
+      if (laser.x < -laser.displayWidth ||
+        laser.x > this.game.config.width + laser.displayWidth ||
+        laser.y < -laser.displayHeight * 4 ||
+        laser.y > this.game.config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+
+    for (var i = 0; i < this.playerLasers.getChildren().length; i++) {
+      var laser = this.playerLasers.getChildren()[i];
+      laser.update();
+
+      if (laser.x < -laser.displayWidth ||
+        laser.x > this.game.config.width + laser.displayWidth ||
+        laser.y < -laser.displayHeight * 4 ||
+        laser.y > this.game.config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
+    }
+  }
+
+  getEnemiesByType(type) {
+    var arr = [];
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+      if (enemy.getData("type") == type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
   }
 }
